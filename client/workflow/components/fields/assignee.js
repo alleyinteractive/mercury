b739fb0fd1fdbf4f1b11b6caaa01b23b44670434
Amy Evans <amy@alley.co>
2019-04-10 10:44:04 -0500

    Set the assignee to the default value, if defined

--- client/workflow/components/fields/assignee.js
+++ client/workflow/components/fields/assignee.js
@@ -2,11 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { getTask } from 'services/tasks';
 import omit from 'lodash/fp/omit';
+import { setMeta } from 'services/meta';
+import useMeta from 'hooks/meta';
 import Select from './select';
 
 const Assignee = (props) => {
-  const { assigneeTaskSlug } = props;
+  const {
+    assigneeTaskSlug,
+    slug,
+  } = props;
   const task = getTask(assigneeTaskSlug);
+
+  // Get the current assignee.
+  const currentAssignee = useMeta(slug);
+
+  // If there's no current assignee, set to the default assignee (if defined).
+  if (! currentAssignee && task.assignees.defaultUser) {
+    setMeta(slug, task.assignees.defaultUser.toString());
+  }
 
   return (
     <Select
@@ -23,6 +36,7 @@ const Assignee = (props) => {
 
 Assignee.propTypes = {
   assigneeTaskSlug: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
 };
 
 export default Assignee;

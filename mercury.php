26e28ea8e0e9b885f888c0aa66abcadce45c078d
Owen Stowe <ostowe@gmail.com>
2019-04-10 12:14:49 -0400

    Plugin version seems like it should match asset version

--- mercury.php
+++ mercury.php
@@ -7,7 +7,7 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     mercury
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         1.1.0
  *
  * @package         Mercury
  */

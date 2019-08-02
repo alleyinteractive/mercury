79c990fee5bc98942d94db7c213e4c6051ef0c09
Zack Rothauser <zack@alley.co>
2019-08-02 09:02:57 -0700

    Starting to set up Docker for codeception

--- tests/acceptance/WPFirstCest.php
+++ tests/acceptance/WPFirstCest.php
@@ -1,13 +1,25 @@
-<?php 
+<?php
 
-class WPFirstCest
-{
-    public function _before(AcceptanceTester $I)
-    {
+class SettingsPageCest {
+	public function _before( AcceptanceTester $I ) {
+		$I->amOnPage( '/wp-login.php' );
+		$I->wait( 1 );
+		$I->fillField( [ 'name' => 'log' ], 'alley' );
+		$I->fillField( [ 'name' => 'pwd' ], 'interactive' );
+		$I->click( '#wp-submit' );
     }
 
-    // tests
-    public function tryToTest(AcceptanceTester $I)
-    {
-    }
+	public function general_settings_page( AcceptanceTester $I ) {
+        $I->amOnPage('/wp-admin/admin.php?page=mercury');
+
+        $I->see('Mercury Settings', 'h1');
+
+        $I->see('Post types', 'a');
+        $I->click(['link' => 'Post types']);
+
+        $I->checkOption('#fm-mercury-0-post_types-0-post_types-0-post');
+        $I->click('Save Changes');
+
+        $I->seeCheckboxIsChecked('#fm-mercury-0-post_types-0-post_types-0-post');
+	}
 }
---
+++ tests/unit/Acme/SomeClassTest.php
@@ -0,0 +1,23 @@
+<?php namespace Acme;
+
+class SomeClassTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}

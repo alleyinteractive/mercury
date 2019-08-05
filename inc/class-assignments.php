de4ebd4fb8ca2de2ddca92a0716f2443ab391163
Amy Evans <amy@alley.co>
2019-08-04 20:12:44 -0500

    Add submenu page for each group queue

--- inc/class-assignments.php
+++ inc/class-assignments.php
@@ -40,6 +40,7 @@ class Assignments {
 			4
 		);
 
+		// Get Mercury post types, and add a submenu page for each.
 		foreach ( get_mercury_post_types() as $post_type ) {
 
 			// Determine the appropriate parent slug.
@@ -59,6 +60,86 @@ class Assignments {
 				},
 			);
 		}
+
+		// Get all usergroups used by tasks, and add a submenu page if the user is a member.
+		foreach ( self::get_user_groups_for_tasks() as $usergroup ) {
+
+			// Bail if the current user isn't a member of this group.
+			if ( ! in_array( get_current_user_id(), $usergroup->user_ids ?? [], true ) ) {
+				continue;
+			}
+
+			$title     = sprintf( __( '%1$s Queue', 'mercury' ), $usergroup->name );
+			$menu_slug = str_replace( 'ef-usergroup', 'mercury-assignments', $usergroup->slug );
+
+			// Add a submenu page for the user group.
+			add_submenu_page(
+				'mercury-assignments',
+				$title,
+				$title,
+				'edit_posts',
+				$menu_slug,
+				function() use ( $usergroup ) {
+					$this->group_queue_page( $usergroup );
+				}
+			);
+		}
+	}
+
+	/**
+	 * Get all Edit Flow usergroups in use by Mercury tasks.
+	 */
+	public static function get_user_groups_for_tasks() {
+		global $edit_flow;
+
+		// Check for a cache hit.
+		$cache_key         = 'mercury_usergroups';
+		$cached_usergroups = get_transient( $cache_key );
+
+		if ( false !== $cached_usergroups ) {
+			return $cached_usergroups;
+		}
+
+		// Get all tasks.
+		$tasks_query = new \WP_Query(
+			[
+				'fields'         => 'ids',
+				'post_type'      => Post_Type::WORKFLOW_TASK_POST_TYPE,
+				'posts_per_page' => -1,
+			]
+		);
+
+		// Get the user groups in use by Mercury tasks.
+		$mercury_group_ids = [];
+		foreach ( ( $tasks_query->posts ?? [] ) as $task_id ) {
+			// Get the assignments meta.
+			$assignments = (array) get_post_meta( $task_id, 'assignments', true );
+
+			if ( empty( $assignments['assignee_selection']['enable_groups'] ?? false ) ) {
+				continue;
+			}
+
+			$mercury_group_ids = array_unique(
+				array_merge(
+					$mercury_group_ids,
+					$assignments['assignee_selection']['filter_groups'] ?? []
+				)
+			);
+		}
+
+		// Get the full term object for each usergroup.
+		$mercury_groups = array_map(
+			function ( $id ) use ( $edit_flow ) {
+				return $edit_flow->user_groups->get_usergroup_by( 'id', $id );
+			},
+			$mercury_group_ids
+		);
+
+		if ( ! empty( $mercury_groups ) ) {
+			set_transient( $cache_key, $mercury_groups, 5 * MINUTE_IN_SECONDS );
+		}
+
+		return $mercury_groups;
 	}
 
 	/**
@@ -86,6 +167,29 @@ class Assignments {
 					<?php
 					$assignments_table->display();
 					$assignments_table->get_user_dropdown();
+					?>
+				</form>
+			</div>
+		<?php
+	}
+
+	/**
+	 * Display the group assignments queue page.
+	 *
+	 * @param \WP_Term $usergroup The usergroup for which to get assignments.
+	 */
+	public function group_queue_page( $usergroup ) {
+		$assignments_table = new GUI\Group_Assignments_Queue( $usergroup );
+		$assignments_table->prepare_items();
+
+		$title = sprintf( __( '%1$s Queue', 'mercury' ), $usergroup->name );
+		?>
+			<div class="wrap">
+				<form method="get">
+					<h2><?php echo esc_html( $title ); ?></h2>
+					<input type="hidden" id="page" name="page" value="<?php echo esc_attr( $assignments_table->get_page() ); ?>">
+					<?php
+					$assignments_table->display();
 					?>
 				</form>
 			</div>
